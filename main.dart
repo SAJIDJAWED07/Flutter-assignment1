448276e46edcb3a5f966e6a404e25184f7753656
SAJID JAWED <sajidjawed206080@gmail.com>
2024-11-28 03:46:20 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,94 @@
+                  ///////------QUESTION NO 1 ------///////
+                
+// Create two integer variables lenght and breadth and assign value then check if they are square values or rectangle values.
+// ie:if both values are equal then its square otherwise rectangle.
+
+                     /////----ANSWER NO 1-----//////
+                
+// void main() {
+//   int length = 15;
+//   int breath = 15;
+
+//   if (length == breath) {
+//     print("It's Square");
+//   } else {
+//     print("It's Rectangle");
+//   }
+// }
+
+                    //////----QUESTION NO 2-----/////////
+                  
+// Take two variables and store age then using if/else condition to determine oldest and youngest them.
+
+                  ///////-----ANSWER NO 2-------/////////
+                 
+// void main() {
+//   int ageOne = 25;
+//   int ageTwo = 35;
+//   if (ageOne > ageTwo) {
+//     print("first person is older");
+//   } else if (ageOne < ageTwo) {
+//     print("Second person is youngest");
+//   }else{
+//     print("Both person are same age");
+
+//   }
+// }
+
+                  ///////-------QUESTION NO 3----------/////////
+                  
+// A student will not be allowed to sit in exam if his attendance is less than 75%.Create integer variable
+// and assign value:Number of classes in held = 16,
+//Number of classes attended = 10,
+//and print percentage of class attended.
+//is student is allowed to sit in exam or not?
+
+                ///////-------ANSWER NO 3----------/////////
+
+// void main() {
+//   int totalClasses = 16;
+//   int attendedClasses = 10;
+
+//   double percentage = (attendedClasses*100) /totalClasses;
+
+//   print("Attendance Percentage:${percentage}%");
+
+//   if(percentage >=75){
+//     print("Student is allow");
+//   }else{
+//     print("Student is not allow");
+//   }
+
+// }
+
+                  ///////-------QUESTION NO 4----------/////////
+                  
+// Write a program to read temperature in centigrade and display a suitable message according to temperature:
+//you have num veriable temperature = 42;
+//now print the message according to temperature:
+//temp < 0 then freezing weather 8
+//temp 0-10 then verycold weather
+//temp 10-20 then cold weather
+//temp 20-30 then normal weather
+//temp 30-40 then hot weather
+//temp >=40 then its very hot weather
+
+
+                    ///////-------ANSWER NO 4----------/////////
+
+// void main() {
+//   num temperature = 42;
+//   if (temperature < 0) {
+//     print("freezing weather");
+//   } else if (temperature > 0 && temperature <= 10) {
+//     print("Very Cold weather");
+//   } else if (temperature > 10 && temperature <= 20) {
+//     print("Cold weather");
+//   } else if (temperature > 20 && temperature <= 30) {
+//     print("Normal weather");
+//   } else if (temperature > 30 && temperature <= 40) {
+//     print("Hot weather");
+//   } else {
+//     print("It's Very Hot weather");
+//   }
+// }
